577a24f54127134ad53d59f09ead3466155061e0
Alejandr0-3nciso <peaencisopi@unal.edu.co>
2021-10-10 21:30:09 -0500

    audio detectado y organizando data frame para clasificacion de voz

--- audio transcript.py
+++ audio transcript.py
@@ -26,10 +26,21 @@ sound = AudioSegment.from_mp3("Debate presidencial Colombia Noticias Caracol.mp3
 table_times=pd.read_excel("speech times per candidate.xlsx")
 
 
+table_times["archivo"]=''
+table_times["texto"]=''
+
+
+table_candidate1=pd.DataFrame()
+#table_candidate["archivo"]=''
+#table_candidate["texto"]=''
+
 for i in set(table_times.candidato):
     table_candidate=table_times[table_times.candidato==i].reset_index()
+    print("Candidato: "+i)
     for j in np.arange(table_candidate.shape[0]):
-    
+        
+        print("Pregunta # ",table_candidate["Pregunta"].iloc[j])
+        
         startMin = table_candidate['min_inicio'].iloc[j]
         startSec = table_candidate['seg_inicio'].iloc[j]
 
@@ -41,20 +52,68 @@ for i in set(table_times.candidato):
         endTime = endMin*60*1000+endSec*1000
 
         extract = sound[startTime:endTime]
-        file_name=i+str(j)+'.wav'
+        file_name=i+str(j+1)+'.wav'
         # Saving
         extract.export( file_name, format="wav")
                      
 
         # use the audio file as the audio source                                        
-        r = sr.Recognizer()
-        with sr.AudioFile(file_name) as source:
-            audio = r.record(source)  # read the entire audio file                  
+        #r = sr.Recognizer()
+        #with sr.AudioFile(file_name) as source:
+        #    audio = r.record(source)  # read the entire audio file                  
 
-            print("Transcription: " + r.recognize_google(audio,language="es-ES"))
+        #    print("Transcription: " + r.recognize_google(audio,language="es-ES"))
+        #texto= r.recognize_google(audio,language="es-ES") 
 
+        table_candidate["archivo"].iloc[j]=file_name
+        #table_candidate["texto"].iloc[j]=texto
+    
+    table_candidate1=table_candidate1.append(table_candidate)
+    
+    
 
+startTime = datetime.now()
+for j in np.arange(table_times.shape[0]):
+        
+    print("Pregunta # ",table_times["Pregunta"].iloc[j])
+    print("Pregunta # ",table_times["Pregunta"].iloc[j])
+        
+    startMin = table_times['min_inicio'].iloc[j]
+    startSec = table_times['seg_inicio'].iloc[j]
 
+    endMin = table_times['min_fin'].iloc[j]
+    endSec = table_times['seg_fin'].iloc[j]
+
+        # Time to miliseconds
+    startTime = startMin*60*1000+startSec*1000
+    endTime = endMin*60*1000+endSec*1000
+
+    extract = sound[startTime:endTime]
+    file_name=table_times["candidato"].iloc[j]+str(j+1)+'.wav'
+        # Saving
+    extract.export( file_name, format="wav")
+                     
+
+        # use the audio file as the audio source                                        
+    r = sr.Recognizer()
+    with sr.AudioFile(file_name) as source:
+        audio = r.record(source)  # read the entire audio file                  
+        print("Transcription: " + r.recognize_google(audio,language="es-ES"))
+        texto= r.recognize_google(audio,language="es-ES") 
+
+    table_times["archivo"].iloc[j]=file_name
+    table_candidate["texto"].iloc[j]=texto
+    
+
+print(datetime.now() - startTime)    
+    
+    
+    
+    table_candidate1=table_candidate1.append(table_candidate)
+
+    
+
+table_times.columns
 
 
 
